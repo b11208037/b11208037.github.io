41588070ef19e882a4f966cc99a6d3874c09b62c
luarn2008 <luarn2008@gmail.com>
2026-05-05 11:42:10 +0800

    Update chatbotb1.js

--- chatbotb1.js
+++ chatbotb1.js
@@ -3,7 +3,7 @@ class ChatBot {
     constructor() {
         // ⚠️ Important: Replace with your n8n webhook URL
         // ****************************************************************
-        this.webhookUrl = 'https://a3g.app.n8n.cloud/webhook/chatb1';
+        this.webhookUrl = 'https://a3g.app.n8n.cloud/webhook/chatb';
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
