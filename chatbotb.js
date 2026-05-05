cf9d6fc4a51f3c4ea2a7d48fc47dc0bfd8a71523
b11208037 <yuki20050828@gmail.com>
2026-05-05 13:08:10 +0800

    Update chatbotb.js

--- chatbotb.js
+++ chatbotb.js
@@ -3,7 +3,7 @@ class ChatBot {
     constructor() {
         // ⚠️ Important: Replace with your n8n webhook URL
         // ****************************************************************
-        this.webhookUrl = 'https://b11208037.app.n8n.cloud/webhook/chatb';
+        this.webhookUrl = 'https://a3g.app.n8n.cloud/webhook/chatb';
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
